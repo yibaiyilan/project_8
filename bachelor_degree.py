ec5e1d94b699ca009cb1c5b2d74b086e3deea534
Xiaobei <yibaiyilan@gmail.com>
2022-08-30 04:53:16 +0000

    add

---
+++ bachelor_degree.py
@@ -0,0 +1,155 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import os
+import plotly as py
+
+# from plotly.graph_objs import *
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+########### Define your variables ######
+
+tabtitle = 'Bachelor Degree'
+sourceurl = 'https://www.kaggle.com/datasets/tjkyner/bachelor-degree-majors-by-age-sex-and-state'
+githublink = 'https://github.com/yibaiyilan/project_8.git'
+# here's the list of possible columns to choose from.
+list_of_columns =['Bachelor\'s Degree Holders','Science and Engineering','Science and Engineering Related Fields','Business','Education','Arts, Humanities and Others']
+
+
+########## Set up the chart
+
+df = pd.read_csv('Bachelor_Degree_Majors.csv',encoding='latin1',on_bad_lines='skip')
+
+########## US States to Code
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+df['Code'] = df['State'].map(us_state_to_abbrev)
+df=df[df['Age Group']!='25 and older']
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
+
+app.layout = html.Div(children=[
+    html.H1('2019 Female Rate for Bachelor Degree, by State'),
+    html.Div([
+        html.Div([
+                html.H6('Select a major for analysis:'),
+                dcc.Dropdown(
+                    id='options-drop',
+                    options=[{'label': i, 'value': i} for i in list_of_columns],
+                    value='Bachelor\'s Degree Holders'
+                ),
+        ], className='two columns'),
+        html.Div([dcc.Graph(id='figure-1'),
+            ], className='ten columns'),
+    ], className='twelve columns'),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
+    ]
+)
+
+
+# make a function that can intake any varname and produce a map.
+@app.callback(Output('figure-1', 'figure'),
+             [Input('options-drop', 'value')])
+def make_figure(varname):
+    mycolorbartitle = "Bachelor Degree Holders"
+    mygraphtitle = f'Female Rate for Bachelor Degree of {varname} in 2019'
+    mycolorscale = 'Sunset' # Note: The error message will list possible color scales.
+    
+    se = pd.DataFrame(df,columns = ['Code','Sex',{varname}])
+    se[{varname}] = se[{varname}].replace(",","", regex=True).astype(float)
+    total_se = se[se['Sex']=='Total'].groupby(['Code'],as_index = False).sum().rename(columns={{varname}:"Total"})
+    female_se = se[se['Sex']=='Female'].groupby(['Code'],as_index = False).sum().rename(columns={{varname}:"Female"})
+    female_rate_se = pd.DataFrame()
+    female_rate_se['Code'] = sex['Code']
+    female_rate_se = pd.merge(female_se,total_se,on=['Code'])
+    female_rate_se['Female Rateß'] = female_rate_se['Female']/female_rate_se['Total']
+    
+    data=go.Choropleth(
+        locations=df['Code'], # Spatial coordinates
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        z = female_rate_se['Female Rate'].astype(float), # Data to be color-coded
+        colorscale = mycolorscale,
+        colorbar_title = mycolorbartitle,
+    )
+    fig = go.Figure(data)
+    fig.update_layout(
+        title_text = mygraphtitle,
+        geo_scope='usa',
+        width=1200,
+        height=800
+    )
+    return fig
+
+
+############ Deploy
+if __name__ == '__main__':
+    app.run_server(debug=True)
